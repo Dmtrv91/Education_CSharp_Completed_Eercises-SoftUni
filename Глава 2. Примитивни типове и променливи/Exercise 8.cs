d2700882bd0e7c7008a9325a89715f48c3bf2ed3
Dimitar Dimitrov <62393797+Dmtrv91@users.noreply.github.com>
2020-05-13 11:42:11 +0300

    Update Exercise 8.cs

--- Глава 2. Примитивни типове и променливи/Exercise 8.cs
+++ Глава 2. Примитивни типове и променливи/Exercise 8.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 
-namespace ConsoleAppGlava2Excercises8
+// Декларирайте две променливи от тип string и им присвоете стойности "Hello" и "World". Декларирайте променлива от тип object и ѝ присвоете
+// стойността на конкатенацията на двете променливи от тип string (не изпускайте интервала по средата). Декларирайте трета променлива от
+// тип string и я инициализирайте със стойността на променливата от тип object.
+
+namespace ConsoleAppGlava2Exercise8
 {
     class Program
     {
