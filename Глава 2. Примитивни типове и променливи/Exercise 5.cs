9b3d8b6384d903ddae7ef4fd18b944f3ae806cb2
Dimitar Dimitrov <62393797+Dmtrv91@users.noreply.github.com>
2020-05-13 11:38:13 +0300

    Update Exercise 5.cs

--- Глава 2. Примитивни типове и променливи/Exercise 5.cs
+++ Глава 2. Примитивни типове и променливи/Exercise 5.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 
-namespace ConsoleAppGlava2exercises5
+//Декларирайте променлива от тип char и присвоете като стойност символа, който има Unicode код 72 (използвайте 
+//калкулатора на Windows за да намерите шестнайсетичното представяне на 72).
+
+namespace ConsoleAppGlava2Exercise5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Декларирайте променлива от тип char и присвоете като стойност символа, 
-            //който има Unicode код 72 (използвайте калкулатора на Windows за да намерите 
-            //шестнайсетичното представяне на 72).
-
             int exr = 0x100;
 
             Console.WriteLine(exr);
