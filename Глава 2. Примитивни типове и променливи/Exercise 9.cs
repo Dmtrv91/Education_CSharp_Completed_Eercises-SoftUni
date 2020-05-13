cbd4e8e9b57ab1a2ffa3db428cf0630559a443da
Dimitar Dimitrov <62393797+Dmtrv91@users.noreply.github.com>
2020-05-13 11:43:22 +0300

    Update Exercise 9.cs

--- Глава 2. Примитивни типове и променливи/Exercise 9.cs
+++ Глава 2. Примитивни типове и променливи/Exercise 9.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 
-namespace ConsoleAppGlava2Excercises9
+// Декларирайте две променливи от тип string и им присвоете стойност "The "use" of quotations causes difficulties." 
+// (без първите и последни кавички). В едната променлива използвайте quoted string, а в другата не го използвайте.
+
+namespace ConsoleAppGlava2Exercise9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //9. Декларирайте две променливи от тип string 
-            //и им присвоете стойност "The "use" of quotations 
-            //causes difficulties." (без първите и последни 
-            //кавички). В едната променлива използвайте quoted
-            //string, а в другата не го използвайте.
-
             string sentence = "of quotations causes difficulties.";
             string The2 =  "\"use\"";
             Console.WriteLine("The " + The2 + sentence);
