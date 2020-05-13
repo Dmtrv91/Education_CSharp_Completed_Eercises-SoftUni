d4c159f13bc880d9e0901eb612021014f35f1af6
dmtrv91 <azrobo91@gmail.com>
2020-05-14 00:18:04 +0300

    Push

---
+++ Глава 3. Примитивни типове и променливи/Exercise 12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppConsoleAppGlava3Excerises12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишете булев израз, който проверява дали битъ
+            //на позиция p на цялото число v има стойност 
+            // Пример v=5, p=1 -> false.
+
+            int v = 5;
+            int p = 1;
+            int i =0;
+            int mask = i << p;
+
+            Console.WriteLine((v & mask) != 0 ? 1 : 0);
+        }
+    }
+}
---
+++ Глава 3. Примитивни типове и променливи/Exercise 13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleAppConsoleAppGlava3Excerises13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This program calculates " +
+            "the area of a reactangle or a triangle");
+
+            Console.WriteLine("Enter a and b (for rectangle) " +
+             "or a and h (for triangle): ");
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter 1 for a rectangle or " +
+            "2 for a triangle: ");
+
+            int choise = int.Parse(Console.ReadLine());
+            double area = (double)(a * b) / choise;
+            Console.WriteLine("The area of your figure is " + area);
+
+        }
+    }
+}
