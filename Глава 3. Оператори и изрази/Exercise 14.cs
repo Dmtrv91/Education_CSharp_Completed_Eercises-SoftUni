c6ffb9a7badf4c80f92673ca17f944934359dc95
dmtrv91 <azrobo91@gmail.com>
2020-05-18 17:22:39 +0300

    Глава 3. Още упражнения.

---
+++ Глава 3. Оператори и изрази/Exercise 14.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//Напишете програма, която проверява дали дадено число n (1 < n < 100) е просто(т.е.се дели без остатък само на себе си и на единица).
+
+namespace ConsoleAppConsoleAppGlava3Exercises14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 72;
+            bool isPrime = true;
+            if (number > 2)
+                for (int i = 2; i <= Math.Ceiling(Math.Sqrt(numbber)); ++i)
+                {
+                    if (number % i == 0) isPrime = false;
+                }
+            Console.WriteLine("{0} is prime?: {1}", number, isPrime);
+        }
+    }
+}
---
+++ Глава 3. Оператори и изрази/Exercise 3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//Напишете израз, който да проверява дали третата цифра (от дясноналяво) на дадено цяло число е 7.
+
+namespace ConsoleAppConsoleAppGlava3Exercises3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 45764;
+            bool isSeven = (number / 100) % 10 == 7 ? true : false;
+            Console.WriteLine("Third digit of {0} is 7", number, even);
+        }
+    }
+}
