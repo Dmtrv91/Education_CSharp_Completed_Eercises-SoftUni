dca5dea43562680a964d664f5ab1a407450a1c30
Dimitar Dimitrov <62393797+Dmtrv91@users.noreply.github.com>
2020-05-18 17:35:53 +0300

    Update Exercise 11.cs

--- Глава 4. Вход и изход от конзолата/Exercise 11.cs
+++ Глава 4. Вход и изход от конзолата/Exercise 11.cs
@@ -2,7 +2,7 @@
 
 //Напишете програма, която отпечатва на конзолата първите 100 числа от редицата на Фибоначи: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,233, 377, ... 
 
-namespace ConsoleAppConsoleAppGlava4Exercises12
+namespace ConsoleAppConsoleAppGlava4Exercises11
 {
     class Program
     {
