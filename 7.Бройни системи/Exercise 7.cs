f1e4ff863ee005f7d6e81c85ef8ae4f5473911c3
dmtrv91 <azrobo91@gmail.com>
2020-05-19 11:43:24 +0300

    7.Бройни системи.

---
+++ 7.Бройни системи/Exercise 7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//Да се напише програма, която преобразува шестнадесетично число в десетично.
+
+namespace ConsoleAppGlava8Exercises7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Hexadecimal number: ");
+            string hexa = (Console.ReadLine());
+
+            Console.WriteLine("{0} to decimal is: {1} ", hexa, Convert.ToInt32(hexa, 16));
+        }
+    }
+}
---
+++ 7.Бройни системи/Exercise 8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//Да се напише програма, която преобразува шестнадесетично число в двоично.
+
+namespace ConsoleAppGlava8Exercises8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter hexadecimal number : ");
+            string hexa = Console.ReadLine();
+            Console.WriteLine("{0} to binary is {1} ", hexa, Convert.ToString(Convert.ToInt32(hexa, 16), 2));
+        }
+    }
+}
