55af5fc6ae0708b17a9e18ab12fa20d826a45149
dmtrv91 <azrobo91@gmail.com>
2020-05-22 14:16:54 +0300

    10. Рекурсия

---
+++ 10. Рекурсия/Exercise 2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//Напишете програма, която симулира изпълнението на n вложени цикъла от 1 до n.
+
+namespace ConsoleAppGlava10Exercises2
+{
+    class Program
+    {
+        static void Loops(int[] arr, int index)
+        {
+            if (index >= arr.Length)
+            {
+                foreach (int element in arr) Console.Write("{0} ", element);
+                Console.WriteLine();
+            }
+            else
+                for (int i = 1; i <= arr.Length; i++)
+                {
+                    arr[index] = i;
+                    Loops(arr, index + 1);
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Loops(arr, 0);
+        }
+    }
+}
---
+++ 10. Рекурсия/Exercise 3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//Напишете рекурсивна програма, която генерира всички комбинации с повторение на n елемента от k-ти клас.
+
+namespace ConsoleAppGlava10Exercises3
+{
+    class Program
+    {
+        static void GetCombinations(int[] arr, int index, int start, int end)
+        {
+            if (index >= arr.Length)
+            {
+                Console.Write("(");
+                for (int i = 0; i < arr.Length; i++)
+                    if (i < arr.Length - 1) Console.Write("{0} ", arr[i]);
+                    else Console.Write(arr[i]);
+                Console.Write("), ");
+            }
+            else
+                for (int i = start; i <= end; i++)
+                {
+                    arr[index] = i;
+                    GetCombinations(arr, index + 1, i, end);
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter K: ");
+            int k = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[k];
+            GetCombinations(arr, 0, 1, n);
+        }
+    }
+}
